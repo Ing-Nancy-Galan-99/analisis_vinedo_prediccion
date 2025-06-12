d943b978c6f2c867fbe134906d16864d5fbca5ae
NANCY JIMENEZ GALAN <nancyjimenezgalan@gmail.com>
2025-06-12 18:34:56 -0500

    Update Matriz_correlacion_calor.R

--- Matriz_correlacion_calor.R
+++ Matriz_correlacion_calor.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(corrplot)
 library(RColorBrewer)
 
-df <- read.csv(datos_vinedo_tratados1.csv") 
+df <- read.csv("C:\\datos_vinedo_tratados1.csv") 
 summary(df)
 
 #verificacion de datos nuelos en la data
